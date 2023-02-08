7a9cb181c5e77b59062dd1145e6c96e0ff94254b
Vladimir Fursov <vladimirfursov228@gmail.com>
2023-02-08 20:53:16 +0200

    completed test task

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,12 @@
-//script
+document.addEventListener('DOMContentLoaded', () => {
+  const panelBtn = document.querySelectorAll('.panel__dropdown-btn');
+
+  panelBtn.forEach(el => {
+    el.addEventListener('click', (e) => {
+      let currentBtn = e.currentTarget;
+      let drop = currentBtn.closest('.panel__dropdown').querySelector('.dropdown');
+
+      drop.classList.toggle('dropdown--active');
+    });
+  });
+});
